21677c106e3474aacfa6cf58ffa2ab4851086dde
Bagus <25834188+contactjavas@users.noreply.github.com>
2019-02-21 02:21:26 +0700

    Style formatting

--- ultimate-dashboard.php
+++ ultimate-dashboard.php
@@ -7,13 +7,17 @@
  * Author: MapSteps
  * Author URI: https://mapsteps.com/
  * Text Domain: ultimate-dashboard
- * Domain Path: /languages
+ * Domain Path: /languagess
+ *
+ * @package Ultimate Dashboard
  */
 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+// exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-// Plugin constants
+// Plugin constants.
 define( 'ULTIMATE_DASHBOARD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ULTIMATE_DASHBOARD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
@@ -32,58 +36,59 @@ function udb_admin_scripts() {
 
 	global $pagenow, $typenow;
 
-	// Widget edit screen & create a new Widget screen
-	if( ( $pagenow == 'post.php' || $pagenow == 'post-new.php' ) && $typenow == 'udb_widgets' ) {
+	$plugin_data = get_plugin_data( __FILE__ );
 
-		wp_register_style( 'font-awesome', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/font-awesome.min.css', false, '4.7.0' );
+	// Widget edit screen & create a new Widget screen.
+	if ( ( 'post.php' === $pagenow || 'post-new.php' === $pagenow ) && 'udb_widgets' === $typenow ) {
+
+		wp_register_style( 'font-awesome', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/font-awesome.min.css', array(), '4.7.0' );
 		wp_enqueue_style( 'font-awesome' );
 
-		wp_register_style( 'ultimate-dashboard-cpt', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/ultimate-dashboard-cpt.css', array(), false );
+		wp_register_style( 'ultimate-dashboard-cpt', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/ultimate-dashboard-cpt.css', array(), $plugin_data['Version'] );
 		wp_enqueue_style( 'ultimate-dashboard-cpt' );
 
-		wp_register_script( 'ultimate-dashboard-cpt', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/js/ultimate-dashboard-cpt.js', array( 'jquery' ), false, true );
+		wp_register_script( 'ultimate-dashboard-cpt', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/js/ultimate-dashboard-cpt.js', array( 'jquery' ), $plugin_data['Version'], true );
 		wp_enqueue_script( 'ultimate-dashboard-cpt' );
 	}
 
-	// Dashboard Widget Overview & Settings
-	if( $pagenow == 'edit.php' && $typenow == 'udb_widgets' ) {
+	// Dashboard Widget Overview & Settings.
+	if ( 'edit.php' === $pagenow && 'udb_widgets' === $typenow ) {
 
-		wp_register_style( 'font-awesome',  ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/font-awesome.min.css', false, '4.7.0' );
+		wp_register_style( 'font-awesome', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/font-awesome.min.css', array(), '4.7.0' );
 		wp_enqueue_style( 'font-awesome' );
 
-		wp_register_style( 'ultimate-dashboard-settings', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/ultimate-dashboard-settings.css', array(), false );
+		wp_register_style( 'ultimate-dashboard-settings', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/ultimate-dashboard-settings.css', array(), $plugin_data['Version'] );
 		wp_enqueue_style( 'ultimate-dashboard-settings' );
 
 	}
 
-	// WordPress Dashboard
-	if( $pagenow == 'index.php' ) {
+	// WordPress Dashboard.
+	if ( 'index.php' === $pagenow ) {
 
-		wp_register_style( 'font-awesome', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/font-awesome.min.css', false, '4.7.0' );
+		wp_register_style( 'font-awesome', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/font-awesome.min.css', array(), '4.7.0' );
 		wp_enqueue_style( 'font-awesome' );
 
-		wp_register_style( 'ultimate-dashboard-index', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/ultimate-dashboard-index.css', array(), false );
+		wp_register_style( 'ultimate-dashboard-index', ULTIMATE_DASHBOARD_PLUGIN_URL . 'assets/css/ultimate-dashboard-index.css', array(), $plugin_data['Version'] );
 		wp_enqueue_style( 'ultimate-dashboard-index' );
-
 	}
 
-	// Highlight PRO Link in Sub-Menu
-	$custom_css = '<style>#adminmenu #menu-posts-udb_widgets a[href="edit.php?post_type=udb_widgets&page=addons"] { color: tomato; }</style>';
-	echo $custom_css;
+	// Highlight PRO Link in Sub-Menu.
+	echo '<style>#adminmenu #menu-posts-udb_widgets a[href="edit.php?post_type=udb_widgets&page=addons"] { color: tomato; }</style>';
 
 }
 add_action( 'admin_enqueue_scripts', 'udb_admin_scripts' );
 
 /**
  * Action Links
+ *
+ * @param string $links Links.
  */
 function udb_add_action_links( $links ) {
-
-	$settings = array( '<a href="' . admin_url( 'edit.php?post_type=udb_widgets&page=settings' ) . '">'. __( 'Settings', 'ultimate-dashboard' ) .'</a>' );
+	$settings = array( '<a href="' . admin_url( 'edit.php?post_type=udb_widgets&page=settings' ) . '">' . __( 'Settings', 'ultimate-dashboard' ) . '</a>' );
 	return array_merge( $links, $settings );
 
 }
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'udb_add_action_links' );
 
-// Required Files
+// Required Files.
 require_once ULTIMATE_DASHBOARD_PLUGIN_DIR . 'inc/ultimate-dashboard-init.php';
