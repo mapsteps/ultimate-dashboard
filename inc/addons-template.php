db77f2e3fe84365675f59bd78c72444a385723cc
David Vongries <connect@mapsteps.com>
2019-03-13 12:36:39 +0100

    optimized PRO settings template

--- inc/addons-template.php
+++ inc/addons-template.php
@@ -10,17 +10,17 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 ?>
 
-<div class="wrap">
+<div class="wrap settingstuff">
 
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
-	<section class="udb-addon">
+	<section class="udb-addon postbox">
 
-		<h3 class="udb-addon-title"><?php _e( 'More Widgets, More Options', 'ultimate-dashboard' ); ?></h3>
+		<h2><?php _e( 'More Widgets, More Options', 'ultimate-dashboard' ); ?></h2>
+
+		<a href="https://ultimatedashboard.io/docs/pro/?utm_source=plugin&utm_medium=pro_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-pro.png"></a>
 
 		<div class="udb-addon-content">
-
-			<a href="https://ultimatedashboard.io/docs/pro/?utm_source=plugin&utm_medium=pro_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-pro.png"></a>
 
 			<p><?php _e( 'Ultimate Dashboard PRO lets you remove 3rd Party Widgets, create Text Widgets, add Tooltips to your Widgets & more!', 'ultimate-dashboard' ); ?></p>
 
@@ -30,13 +30,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	</section>
 
-	<section class="udb-addon">
+	<section class="udb-addon postbox">
 
-		<h3 class="udb-addon-title"><?php _e( 'Video Widget', 'ultimate-dashboard' ); ?></h3>
+		<h2><?php _e( 'Video Widget', 'ultimate-dashboard' ); ?></h2>
+
+		<a href="https://ultimatedashboard.io/docs/video-widget/?utm_source=plugin&utm_medium=video_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-video-widget.png"></a>
 
 		<div class="udb-addon-content">
-
-			<a href="https://ultimatedashboard.io/docs/video-widget/?utm_source=plugin&utm_medium=video_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-video-widget.png"></a>
 
 			<p><?php _e( 'Add YouTube & Vimeo Video Widgets that open your video in a responsive lightbox on the WordPress Dashboard.', 'ultimate-dashboard' ); ?></p>
 
@@ -46,13 +46,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	</section>
 
-	<section class="udb-addon">
+	<section class="udb-addon postbox">
 
-		<h3 class="udb-addon-title"><?php _e( 'Multisite Support', 'ultimate-dashboard' ); ?></h3>
+		<h2><?php _e( 'Multisite Support', 'ultimate-dashboard' ); ?></h2>
+
+		<a href="https://ultimatedashboard.io/docs/multisite-support/?utm_source=plugin&utm_medium=multisite_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-multisite-support.png"></a>
 
 		<div class="udb-addon-content">
-
-			<a href="https://ultimatedashboard.io/docs/multisite-support/?utm_source=plugin&utm_medium=multisite_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-multisite-support.png"></a>
 
 			<p><?php _e( 'Take your WordPress multisite network to the next level with the Ultimate Dashboard PRO. Display the same Widgets throughout all sites, reorder Widgets globally & more!', 'ultimate-dashboard' ); ?></p>
 
@@ -62,13 +62,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	</section>
 
-	<section class="udb-addon">
+	<section class="udb-addon postbox">
 
-		<h3 class="udb-addon-title"><?php _e( 'Branding', 'ultimate-dashboard' ); ?></h3>
+		<h2><?php _e( 'Branding', 'ultimate-dashboard' ); ?></h2>
+
+		<a href="https://ultimatedashboard.io/docs/custom-branding/?utm_source=plugin&utm_medium=branding_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-branding.png"></a>
 
 		<div class="udb-addon-content">
-
-			<a href="https://ultimatedashboard.io/docs/custom-branding/?utm_source=plugin&utm_medium=branding_widget&utm_campaign=udb" target="_blank"><img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>assets/img/ultimate-dashboard-branding.png"></a>
 
 			<p><?php _e( 'Customize the WordPress admin and login screen to match your or your clients brand!', 'ultimate-dashboard' ); ?></p>
 
