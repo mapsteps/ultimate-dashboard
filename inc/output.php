43d84311b8de71f3844e461e8ded72698ebdb142
Bagus <25834188+contactjavas@users.noreply.github.com>
2019-03-19 22:01:18 +0700

    style formatting

--- inc/output.php
+++ inc/output.php
@@ -4,65 +4,64 @@
  *
  * @package Ultimate Dashboard
  */
- 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 /**
  * Ultimate Dashboard Widget Output
  */
 function udb_add_dashboard_widgets() {
-
-	// Custom Post Type Loop
+	// Custom Post Type Loop.
 	$args = array(
 		'post_type'      => 'udb_widgets',
 		'posts_per_page' => 100,
 	);
 	$loop = new WP_Query( $args );
-	while ( $loop->have_posts() ) : $loop->the_post();
 
-	// vars
-	$id            = get_the_ID();
-	$title         = get_the_title();
-	$icon          = get_post_meta( $id, 'udb_icon_key', true );
-	$link          = get_post_meta( $id, 'udb_link', true );
-	$target        = get_post_meta( $id, 'udb_link_target', true );
-	$tooltip       = get_post_meta( $id, 'udb_tooltip', true );
-	$position      = get_post_meta( $id, 'udb_position_key', true );
-	$priority      = get_post_meta( $id, 'udb_priority_key', true );
-	$widget_type   = get_post_meta( $id, 'udb_widget_type', true ); // do we need a backwards compatibility check here as we move the settings over in backwards-compatibility.php on init
-	$content       = get_post_meta( $id, 'udb_content', true );
-	$contentheight = get_post_meta( $id, 'udb_content_height', true ) ? ' data-udb-content-height="'. get_post_meta( $id, 'udb_content_height', true ) .'"' : '';
-	$html          = get_post_meta( $id, 'udb_html', true );
+	while ( $loop->have_posts() ) :
+		$loop->the_post();
 
-	// HTML Widget
-	if ( 'html' === $widget_type ) {
+		// vars.
+		$id            = get_the_ID();
+		$title         = get_the_title();
+		$icon          = get_post_meta( $id, 'udb_icon_key', true );
+		$link          = get_post_meta( $id, 'udb_link', true );
+		$target        = get_post_meta( $id, 'udb_link_target', true );
+		$tooltip       = get_post_meta( $id, 'udb_tooltip', true );
+		$position      = get_post_meta( $id, 'udb_position_key', true );
+		$priority      = get_post_meta( $id, 'udb_priority_key', true );
+		$widget_type   = get_post_meta( $id, 'udb_widget_type', true ); // do we need a backwards compatibility check here as we move the settings over in backwards-compatibility.php on init
+		$content       = get_post_meta( $id, 'udb_content', true );
+		$contentheight = get_post_meta( $id, 'udb_content_height', true ) ? ' data-udb-content-height="' . get_post_meta( $id, 'udb_content_height', true ) . '"' : '';
+		$html          = get_post_meta( $id, 'udb_html', true );
 
-		$output = do_shortcode( '<div class="udb-html-wrapper">'. $html .'</div>' );
+		// HTML Widget.
+		if ( 'html' === $widget_type ) {
 
-	// Text Widget
-	} elseif ( 'text' === $widget_type ) {
+			$output = do_shortcode( '<div class="udb-html-wrapper">' . $html . '</div>' );
 
-		$output = do_shortcode( '<div class="udb-content-wrapper"'. $contentheight .'>'. wpautop( $content ) .'</div>' );
+			// Text Widget.
+		} elseif ( 'text' === $widget_type ) {
 
-	// Icon Widget
-	} elseif ( 'icon' === $widget_type ) {
+			$output = do_shortcode( '<div class="udb-content-wrapper"' . $contentheight . '>' . wpautop( $content ) . '</div>' );
 
-		// Widget Output
-		$output = '<a href="'. $link .'" target="'. $target .'"><i class="' . $icon . '"></i></a>';
+			// Icon Widget.
+		} elseif ( 'icon' === $widget_type ) {
 
-		if ( $tooltip ) {
-			$output .= '<i class="udb-info"></i><div class="udb-tooltip"><span>'. $tooltip .'</span></div>';
+			// Widget Output.
+			$output = '<a href="' . $link . '" target="' . $target . '"><i class="' . $icon . '"></i></a>';
+
+			if ( $tooltip ) {
+				$output .= '<i class="udb-info"></i><div class="udb-tooltip"><span>' . $tooltip . '</span></div>';
+			}
 		}
 
-	}
+		$function = function() use ( $output ) {
+			echo $output;
+		};
 
-	$function = function() use ( $output ) {
-		echo $output;
-	};
-
-	// Add Meta Box
-	add_meta_box( 'ms-udb' . $id, $title, $function, 'dashboard', $position, $priority );
+		// Add Meta Box.
+		add_meta_box( 'ms-udb' . $id, $title, $function, 'dashboard', $position, $priority );
 
 	endwhile;
 
@@ -70,33 +69,31 @@ function udb_add_dashboard_widgets() {
 add_action( 'wp_dashboard_setup', 'udb_add_dashboard_widgets' );
 
 /**
- * Remove Default WordPress Dashboard Widgets
+ * Remove Default WordPress Dashboard Widgets.
  */
 function udb_remove_default_dashboard_widgets() {
 
-	// vars
+	// vars.
 	$saved_widgets   = udb_get_saved_default_widgets();
 	$default_widgets = udb_get_default_widgets();
 	$udb_settings    = get_option( 'udb_settings' );
 
-	if( isset( $udb_settings['remove-all'] ) ) {
+	if ( isset( $udb_settings['remove-all'] ) ) {
 
 		remove_action( 'welcome_panel', 'wp_welcome_panel' );
 
 		foreach ( $default_widgets as $id => $widget ) {
 			remove_meta_box( $id, 'dashboard', $widget['context'] );
 		}
-  
-    } else {
+	} else {
 
-		if( isset( $udb_settings['welcome_panel'] ) ) {
-			remove_action('welcome_panel', 'wp_welcome_panel');
+		if ( isset( $udb_settings['welcome_panel'] ) ) {
+			remove_action( 'welcome_panel', 'wp_welcome_panel' );
 		}
 
 		foreach ( $saved_widgets as $id => $widget ) {
 			remove_meta_box( $id, 'dashboard', $widget['context'] );
 		}
-
 	}
 }
 add_action( 'wp_dashboard_setup', 'udb_remove_default_dashboard_widgets', 100 );
@@ -108,7 +105,9 @@ function udb_add_dashboard_css() {
 
 	$udb_pro_settings = get_option( 'udb_pro_settings' );
 
-	if ( !isset( $udb_pro_settings['custom_css'] ) ) return;
+	if ( ! isset( $udb_pro_settings['custom_css'] ) ) {
+		return;
+	}
 
 	$custom_css = $udb_pro_settings['custom_css'];
 
