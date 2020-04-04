e65bcca3c4670aa0af92a80d3606e5a8b3a898d9
David Vongries <connect@mapsteps.com>
2020-04-04 22:05:41 +0200

    should be included in the last commit

--- modules/login-customizer/assets/js/controls.js
+++ modules/login-customizer/assets/js/controls.js
@@ -171,7 +171,7 @@ if (!String.prototype.includes) {
 	function insertProLink() {
 		var proLink = '\
 		<li class="accordion-section control-section udb-pro-control-section">\
-			<a href="https://ultimatedashboard.io/pro/?utm_source=plugin&utm_medium=login_customizer_link&utm_campaign=udb" class="accordion-section-title" target="_blank" tabindex="0">\
+			<a href="https://ultimatedashboard.io/docs/login-customizer/?utm_source=plugin&utm_medium=login_customizer_link&utm_campaign=udb" class="accordion-section-title" target="_blank" tabindex="0">\
 				PRO Features available! ›\
 			</a>\
 		</li>\
