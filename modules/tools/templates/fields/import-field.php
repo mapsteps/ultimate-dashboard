6a33a79436d4e28c2dd184bb9847cfb4544cebea
mapsteps <connect@mapsteps.com>
2020-10-06 15:05:52 +0200

    tweak

--- modules/tools/templates/fields/import-field.php
+++ modules/tools/templates/fields/import-field.php
@@ -11,8 +11,7 @@ return function () {
 
 	?>
 
-	<p><?php _e( '	
-Select the Widgets JSON file you would like to import. When you click the import button below, Ultimate Dashboard will import the widgets.', 'ultimate-dashboard' ); ?></p>
+	<p><?php _e( 'Select the Widgets JSON file you would like to import. When you click the import button below, Ultimate Dashboard will import the widgets.', 'ultimate-dashboard' ); ?></p>
 
 	<div class="fields-wrapper has-top-gap">
 		<label class="block-label" for="udb_import_file"><?php _e( 'Select File', 'ultimate-dashboard' ); ?></label>
