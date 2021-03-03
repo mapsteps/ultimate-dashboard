c31133cc2d0db5a09356edb85f676baa83a3efba
mapsteps <connect@mapsteps.com>
2021-03-03 12:31:16 +0100

    added advertising section to settings page

--- modules/setting/templates/settings-template.php
+++ modules/setting/templates/settings-template.php
@@ -68,7 +68,62 @@ return function () {
 
 			</form>
 
+			<?php if ( ! udb_is_pro_active() ) { ?>
+
+			<div class="heatbox-divider"></div>
+
+			<?php } ?>
+
 		</div>
+
+		<?php if ( ! udb_is_pro_active() ) { ?>
+
+		<div class="heatbox-container heatbox-container-wide heatbox-container-center featured-products">
+
+			<h2><?php _e( 'Check out our other free WordPress products!', 'ultimate-dashboard' ); ?></h2>
+
+			<ul class="products">
+				<li class="heatbox">
+					<a href="https://wordpress.org/plugins/swift-control/" target="_blank">
+						<img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>/assets/img/swift-control.jpg">
+					</a>
+					<div class="heatbox-content">
+						<h3><?php _e( 'WP Swift Control', 'ultimate-dashboard' ); ?></h3>
+						<p class="subheadline"><?php _e( 'Replace the boring WordPress Admin Bar with this!', 'ultimate-dashboard' ); ?></p>
+						<p><?php _e( 'Swift Control is the plugin that make your clients love WordPress. It drastically improves the user experience when working with WordPress and allows you to replace the boring WordPress admin bar with your own navigation panel.', 'ultimate-dashboard' ); ?></p>
+						<a href="https://wordpress.org/plugins/swift-control/" target="_blank" class="button"><?php _e( 'View Features', 'ultimate-dashboard' ); ?></a>
+					</div>
+				</li>
+
+				<li class="heatbox">
+					<a href="https://wordpress.org/themes/page-builder-framework/" target="_blank">
+						<img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>/assets/img/page-builder-framework.jpg">
+					</a>
+					<div class="heatbox-content">
+						<h3><?php _e( 'Page Builder Framework', 'ultimate-dashboard' ); ?></h3>
+						<p class="subheadline"><?php _e( 'The only Theme you\'ll ever need.', 'ultimate-dashboard' ); ?></p>
+						<p class="description"><?php _e( 'With its minimalistic design the Page Builder Framework theme is the perfect foundation for your next project. Build blazing fast websites with a theme that is easy to use, lightweight & highly customizable.', 'ultimate-dashboard' ); ?></p>
+						<a href="https://wordpress.org/themes/page-builder-framework/" target="_blank" class="button"><?php _e( 'View Features', 'ultimate-dashboard' ); ?></a>
+					</div>
+				</li>
+				<li class="heatbox">
+					<a href="https://wordpress.org/plugins/responsive-youtube-vimeo-popup/" target="_blank">
+						<img src="<?php echo esc_url( ULTIMATE_DASHBOARD_PLUGIN_URL ); ?>/assets/img/wp-video-popup.jpg">
+					</a>
+					<div class="heatbox-content">
+						<h3><?php _e( 'WP Video Popup', 'ultimate-dashboard' ); ?></h3>
+						<p class="subheadline"><?php _e( 'The #1 Video Popup Plugin for WordPress.', 'ultimate-dashboard' ); ?></p>
+						<p><?php _e( 'Add beautiful responsive YouTube & Vimeo video lightbox popups to any post, page or custom post type of website without sacrificing performance.', 'ultimate-dashboard' ); ?></p>
+						<a href="https://wordpress.org/plugins/responsive-youtube-vimeo-popup/" target="_blank" class="button"><?php _e( 'View Features', 'ultimate-dashboard' ); ?></a>
+					</div>
+				</li>
+			</ul>
+
+			<p class="credit"><?php _e( 'Made with ❤ in Aschaffenburg, Germany', 'ultimate-dashboard' ); ?></p>
+
+		</div>
+
+		<?php } ?>
 
 	</div>
 
