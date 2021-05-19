ebead773bfdc79e5c046e964cf0324d57f7a6638
Bagus <contactjavas@gmail.com>
2021-05-20 00:28:08 +0700

    let's not changing the user id as other plugins might be using it

--- helpers/class-user-helper.php
+++ helpers/class-user-helper.php
@@ -16,14 +16,22 @@ class User_Helper {
 	/**
 	 * Simulate user with specified role.
 	 *
+	 * Use this wisely.
+	 * If you're sure this function will only run on specific condition,
+	 * then you can omit the 2nd parameter (keep it `false`).
+	 *
 	 * @param string $role_key The role key.
+	 * @param bool   $change_user_id Whether or not to change the user ID.
 	 */
-	public function simulate_role( $role_key ) {
+	public function simulate_role( $role_key, $change_user_id = false ) {
 		global $current_user;
 
 		$role = get_role( $role_key );
 
-		$current_user->ID      = PHP_INT_MAX;
+		if ( $change_user_id ) {
+			$current_user->ID = PHP_INT_MAX;
+		}
+
 		$current_user->allcaps = $role->capabilities;
 		$current_user->caps    = $role->capabilities;
 		$current_user->roles   = array( $role_key );
