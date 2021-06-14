c7595096c13fa0c44e3890c3515238f9addd36ef
Bagus <contactjavas@gmail.com>
2021-06-15 02:33:47 +0700

    remove un-used field

--- modules/branding/templates/fields/admin-submenu-bg-color.php
+++ modules/branding/templates/fields/admin-submenu-bg-color.php
@@ -13,6 +13,18 @@ return function () {
 
 	<input type="text" name="" value="#2c3338" class="udb-color-field udb-branding-color-field udb-instant-preview-trigger" data-default="#2c3338" data-udb-trigger-name="admin-submenu-bg-color" />
 
+	<br>
+
+	<div class="udb-pro-settings-page-notice">
+
+		<p><?php _e( 'This is only preview. Admin Colors feature is available in Ultimate Dashboard PRO.' ); ?></p>
+
+		<a href="https://ultimatedashboard.io/pro/?utm_source=plugin&utm_medium=white_label_link&utm_campaign=udb" class="button button-primary" target="_blank">
+			<?php _e( 'Get Ultimate Dashboard PRO', 'ultimate-dashboard' ); ?>
+		</a>
+
+	</div>
+
 	<?php
 
 };
