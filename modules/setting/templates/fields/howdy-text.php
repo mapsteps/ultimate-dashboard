1a793fa679e14cf62c667d6e3bf33465e60bc16e
Bagus <contactjavas@gmail.com>
2021-07-05 10:36:36 +0700

    add feature: change howdy text

---
+++ modules/setting/templates/fields/howdy-text.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Howdy text field.
+ *
+ * @package Ultimate_Dashboard
+ */
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
+
+return function () {
+
+	$settings = get_option( 'udb_settings' );
+	$headline = isset( $settings['howdy_text'] ) ? $settings['howdy_text'] : '';
+	?>
+
+	<input type="text" name="udb_settings[howdy_text]" class="all-options" value="<?php echo esc_attr( $headline ); ?>" placeholder="<?php _e( 'Howdy', 'ultimate-dashboard' ); ?>" />
+
+	<?php
+
+};
