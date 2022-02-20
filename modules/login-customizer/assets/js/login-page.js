024a5df0a618238406acd0de2524e6d9f5bc8a88
Bagus <25834188+contactjavas@users.noreply.github.com>
2022-02-20 19:21:47 +0700

    support new WP language switcher

---
+++ modules/login-customizer/assets/js/login-page.js
@@ -0,0 +1,17 @@
+(function () {
+	function init() {
+		moveLanguageSwitcher();
+	}
+
+	function moveLanguageSwitcher() {
+		var loginDiv = document.querySelector('#login');
+		if (!loginDiv) return;
+
+		var langSwitcher = document.querySelector(".language-switcher");
+		if (!langSwitcher) return;
+
+		loginDiv.appendChild(langSwitcher);
+	}
+
+	init();
+})();
