fc81f7aa72a55b65ab1788266c9ff9f3eb345943
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-11-02 17:28:15 +0700

    don't use esc_html to output css markups

--- modules/setting/templates/fields/custom-dashboard-css.php
+++ modules/setting/templates/fields/custom-dashboard-css.php
@@ -12,10 +12,19 @@ return function () {
 	$settings   = get_option( 'udb_settings' );
 	$custom_css = isset( $settings['custom_css'] ) ? $settings['custom_css'] : '';
 	$custom_css = wp_strip_all_tags( $custom_css );
+	$custom_css = wp_filter_nohtml_kses( $custom_css );
+	$custom_css = strtr(
+		$custom_css,
+		array(
+			'&gt;' => '>',
+			"\'"   => "'",
+			'\"'   => '"',
+		)
+	);
 
 	?>
 
-	<textarea id="udb-custom-dashboard-css" class="widefat textarea udb-custom-css" name="udb_settings[custom_css]"><?php echo esc_html( $custom_css ); ?></textarea>
+	<textarea id="udb-custom-dashboard-css" class="widefat textarea udb-custom-css" name="udb_settings[custom_css]"><?php echo $custom_css; ?></textarea>
 
 	<?php
 
