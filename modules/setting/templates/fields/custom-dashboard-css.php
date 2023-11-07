cfccccaf84b1bfbcfa506c552b6d8901855f0852
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-11-07 14:13:07 +0700

    bring back old value for compatibility

--- modules/setting/templates/fields/custom-dashboard-css.php
+++ modules/setting/templates/fields/custom-dashboard-css.php
@@ -10,22 +10,24 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 return function () {
 
 	$settings   = get_option( 'udb_settings' );
-	$custom_css = isset( $settings['custom_css'] ) ? $settings['custom_css'] : '';
-	$custom_css = wp_strip_all_tags( $custom_css );
-	$custom_css = wp_filter_nohtml_kses( $custom_css );
-	$custom_css = strtr(
-		$custom_css,
-		array(
-			'&gt;' => '>',
-			"\'"   => "'",
-			'\"'   => '"',
-		)
-	);
+	$custom_css = isset( $settings['custom_css'] ) ? $settings['custom_css'] : false;
+
+	if ( $custom_css ) {
+		$custom_css = wp_strip_all_tags( $custom_css );
+		$custom_css = wp_filter_nohtml_kses( $custom_css );
+		$custom_css = strtr(
+			$custom_css,
+			array(
+				'&gt;' => '>',
+				"\'"   => "'",
+				'\"'   => '"',
+			)
+		);
+	}
 
 	?>
 
 	<textarea id="udb-custom-dashboard-css" class="widefat textarea udb-custom-css" name="udb_settings[custom_css]"><?php echo $custom_css; ?></textarea>
 
 	<?php
-
 };
