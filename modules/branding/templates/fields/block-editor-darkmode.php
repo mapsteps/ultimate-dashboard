ffa53c4f113214f5a9e4168837735a23bf4724fa
mapsteps <connect@mapsteps.com>
2024-06-19 21:19:37 +0200

    remove PRO nag if PRO version is active

--- modules/branding/templates/fields/block-editor-darkmode.php
+++ modules/branding/templates/fields/block-editor-darkmode.php
@@ -39,11 +39,15 @@ return function () {
 
 	<div class="udb-pro-settings-page-notice">
 
-		<p><?php _e( 'This feature is available in Ultimate Dashboard PRO.' ); ?></p>
+		<?php if ( ! udb_is_pro_active() ) : ?>
 
-		<a href="https://ultimatedashboard.io/pro/?utm_source=plugin&utm_medium=white_label_link&utm_campaign=udb" class="button button-primary" target="_blank">
-			<?php _e( 'Get Ultimate Dashboard PRO', 'ultimate-dashboard' ); ?>
-		</a>
+			<p><?php _e( 'This feature is available in Ultimate Dashboard PRO.' ); ?></p>
+
+			<a href="https://ultimatedashboard.io/pro/?utm_source=plugin&utm_medium=white_label_link&utm_campaign=udb" class="button button-primary" target="_blank">
+				<?php _e( 'Get Ultimate Dashboard PRO', 'ultimate-dashboard' ); ?>
+			</a>
+
+		<?php endif; ?>
 
 	</div>
 
