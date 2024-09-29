09fd25f76139378de7d1a7abf6fa2acd67eda4a1
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-09-29 13:03:23 +0700

    and finally move tiny-slider.css to udb's root assets because it's used by 2 modules

--- modules/onboarding-wizard/inc/css-enqueue.php
+++ modules/onboarding-wizard/inc/css-enqueue.php
@@ -11,16 +11,17 @@ return function ( $module ) {
 
 	if ( $module->screen()->is_wizard() ) {
 
+		// Select2 CSS.
+		wp_enqueue_style( 'select2', ULTIMATE_DASHBOARD_PLUGIN_URL . '/assets/css/select2.min.css', array(), ULTIMATE_DASHBOARD_PLUGIN_VERSION );
+
 		// Heatbox.
 		wp_enqueue_style( 'heatbox', ULTIMATE_DASHBOARD_PLUGIN_URL . '/assets/css/heatbox.css', array(), ULTIMATE_DASHBOARD_PLUGIN_VERSION );
 
-		wp_enqueue_style( 'tiny-slider', ULTIMATE_DASHBOARD_PLUGIN_URL . '/modules/onboarding-wizard/assets/css/tiny-slider.css', array(), ULTIMATE_DASHBOARD_PLUGIN_VERSION );
+		// Tiny slider.
+		wp_enqueue_style( 'tiny-slider', ULTIMATE_DASHBOARD_PLUGIN_URL . '/assets/css/tiny-slider.css', array(), ULTIMATE_DASHBOARD_PLUGIN_VERSION );
 
 		// Onboarding Wizard.
 		wp_enqueue_style( 'udb-wizard', ULTIMATE_DASHBOARD_PLUGIN_URL . '/modules/onboarding-wizard/assets/css/onboarding-wizard.css', array(), ULTIMATE_DASHBOARD_PLUGIN_VERSION );
-
-		// Select2 CSS.
-		wp_enqueue_style( 'select2', ULTIMATE_DASHBOARD_PLUGIN_URL . '/assets/css/select2.min.css', array(), ULTIMATE_DASHBOARD_PLUGIN_VERSION );
 
 	}
 
