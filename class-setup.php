faa14d391e4652d987d9468c0b7af784cced0043
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-11-22 16:57:03 +0700

    fix issue where BFCM 2024 notice wasn't dismissable

--- class-setup.php
+++ class-setup.php
@@ -546,7 +546,7 @@ class Setup {
 			wp_send_json_error( 'Invalid Request' );
 		}
 
-		update_option( 'udb_bfcm_notice_dismissed_2023', 1 );
+		update_option( 'udb_bfcm_notice_dismissed_2024', 1 );
 		wp_send_json_success( 'Review notice has been dismissed.' );
 
 	}
