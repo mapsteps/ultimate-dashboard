895dcb777a32e9a8ed8761b91f9f45e90ba796ec
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-02-26 17:01:58 +0700

    improve dom-utils

--- modules/base/assets/js/dom-utils.js
+++ modules/base/assets/js/dom-utils.js
@@ -1,73 +1,193 @@
 /**
  * Find an HTML element by selector.
  *
- * @param {string} selector The selector.
+ * @param {Element|string|null} parentEl The parent element or selector.
+ * @param {string|null} [selector] The selector.
+ *
  * @returns {HTMLElement | null} The element or null if not found.
  */
-export function findHtmlEl(selector) {
-	const el = document.querySelector(selector);
+export function findHtmlEl(parentEl, selector) {
+	/** @type {HTMLElement | null} */
+	let el = null;
 
-	if (el instanceof HTMLElement) {
+	if (parentEl instanceof Element) {
+		if (!selector) {
+			return null;
+		}
+
+		el = parentEl.querySelector(selector);
+
+		if (!(el instanceof HTMLElement)) {
+			return null;
+		}
+
 		return el;
 	}
 
-	return null;
+	if (typeof parentEl !== "string") {
+		return null;
+	}
+
+	el = document.querySelector(parentEl);
+
+	if (!(el instanceof HTMLElement)) {
+		return null;
+	}
+
+	return el;
 }
 
 /**
  * Find HTML elements by selector.
  *
- * @param {string} selector The selector.
+ * @param {Element|string|null} parentEl The parent element or selector.
+ * @param {string|null} [selector] The selector.
+ *
  * @returns {HTMLElement[]} The HTML elements.
  */
-export function findHtmlEls(selector) {
-	const nodes = document.querySelectorAll(selector);
-	const els = [];
+export function findHtmlEls(parentEl, selector) {
+	/** @type {NodeListOf<Element>} */
+	let els;
 
-	for (let i = 0; i < nodes.length; i++) {
-		const el = nodes[i];
+	/** @type {HTMLElement[]} */
+	let htmlEls = [];
+
+	if (parentEl instanceof Element) {
+		if (!selector) {
+			return [];
+		}
+
+		els = parentEl.querySelectorAll(selector);
+
+		for (let i = 0; i < els.length; i++) {
+			const el = els[i];
+
+			if (el instanceof HTMLElement) {
+				htmlEls.push(el);
+			}
+		}
+
+		return htmlEls;
+	}
+
+	if (typeof parentEl !== "string") {
+		return [];
+	}
+
+	els = document.querySelectorAll(parentEl);
+
+	for (let i = 0; i < els.length; i++) {
+		const el = els[i];
 
 		if (el instanceof HTMLElement) {
-			els.push(el);
+			htmlEls.push(el);
 		}
 	}
 
-	return els;
+	return htmlEls;
+}
+
+/**
+ * Find an HTML form element by selector.
+ *
+ * @param {string} selector The selector.
+ * @returns {HTMLFormElement | null} The element or null if not found.
+ */
+export function findFormEl(selector) {
+	const el = document.querySelector(selector);
+
+	if (!(el instanceof HTMLFormElement)) {
+		return null;
+	}
+
+	return el;
 }
 
 /**
  * Find an HTML input element by selector.
  *
- * @param {string} selector The selector.
+ * @param {Element|string|null} parentEl The parent element or selector.
+ * @param {string|null} [selector] The selector.
+ *
  * @returns {HTMLInputElement | null} The element or null if not found.
  */
-export function findInputEl(selector) {
-	const el = document.querySelector(selector);
+export function findInputEl(parentEl, selector) {
+	/** @type {HTMLInputElement | null} */
+	let el = null;
 
-	if (el instanceof HTMLInputElement) {
+	if (parentEl instanceof Element) {
+		if (!selector) {
+			return null;
+		}
+
+		el = parentEl.querySelector(selector);
+
+		if (!(el instanceof HTMLInputElement)) {
+			return null;
+		}
+
 		return el;
 	}
 
-	return null;
+	if (typeof parentEl !== "string") {
+		return null;
+	}
+
+	el = document.querySelector(parentEl);
+
+	if (!(el instanceof HTMLInputElement)) {
+		return null;
+	}
+
+	return el;
 }
 
 /**
  * Find HTML input elements by selector.
  *
- * @param {string} selector The selector.
+ * @param {Element|string|null} parentEl The parent element or selector.
+ * @param {string|null} [selector] The selector.
+ *
  * @returns {HTMLInputElement[]} The HTML input elements.
  */
-export function findInputEls(selector) {
-	const nodes = document.querySelectorAll(selector);
-	const els = [];
+export function findInputEls(parentEl, selector) {
+	/** @type {NodeListOf<Element>} */
+	let els;
 
-	for (let i = 0; i < nodes.length; i++) {
-		const el = nodes[i];
+	/** @type {HTMLInputElement[]} */
+	let inputEls = [];
+
+	if (parentEl instanceof Element) {
+		if (!selector) {
+			return [];
+		}
+
+		els = parentEl.querySelectorAll(selector);
+
+		for (let i = 0; i < els.length; i++) {
+			const el = els[i];
+
+			if (el instanceof HTMLInputElement) {
+				inputEls.push(el);
+			}
+		}
+
+		return inputEls;
+	}
+
+	if (typeof parentEl !== "string") {
+		return [];
+	}
+
+	els = document.querySelectorAll(parentEl);
+
+	for (let i = 0; i < els.length; i++) {
+		const el = els[i];
 
 		if (el instanceof HTMLInputElement) {
-			els.push(el);
+			inputEls.push(el);
 		}
 	}
 
-	return els;
+	return inputEls;
 }
