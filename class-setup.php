449d5f202e5a70f27579a515bd0a98f786ff7334
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-02-27 22:07:28 +0700

    simplify un-necessary step

--- class-setup.php
+++ class-setup.php
@@ -428,6 +428,7 @@ class Setup {
 		$review_url = 'https://wordpress.org/support/plugin/ultimate-dashboard/reviews/?rate=5#new-post';
 		$link_start = '<a href="' . $review_url . '" target="_blank">';
 		$link_end   = '</a>';
+
 		// translators: %1$s: Emoji, %2$s: Link start tag, %3$s: Link end tag.
 		$notice   = sprintf( __( '%1$s Love using Ultimate Dashboard? - That\'s Awesome! Help us spread the word and leave us a %2$s 5-star review %3$s in the WordPress repository.', 'ultimate-dashboard' ), $emoji, $link_start, $link_end );
 		$btn_text = __( 'Sure! You deserve it!', 'ultimate-dashboard' );
@@ -445,9 +446,7 @@ class Setup {
 	 */
 	public function dismiss_review_notice() {
 
-		$dismiss = isset( $_POST['dismiss'] ) ? absint( $_POST['dismiss'] ) : 0;
-
-		if ( empty( $dismiss ) ) {
+		if ( isset( $_POST['dismiss'] ) ) {
 			wp_send_json_error( 'Invalid Request' );
 		}
 
@@ -540,9 +539,7 @@ class Setup {
 	 */
 	public function dismiss_bfcm_notice() {
 
-		$dismiss = isset( $_POST['dismiss'] ) ? absint( $_POST['dismiss'] ) : 0;
-
-		if ( empty( $dismiss ) ) {
+		if ( isset( $_POST['dismiss'] ) ) {
 			wp_send_json_error( 'Invalid Request' );
 		}
 
