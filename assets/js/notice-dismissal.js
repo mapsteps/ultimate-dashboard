e00450c6ef9a87d2b4eafc684976f1c31d597503
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-02-27 21:44:24 +0700

    notice-dimissal.js type check

--- assets/js/notice-dismissal.js
+++ assets/js/notice-dismissal.js
@@ -1,31 +1,35 @@
 (function ($) {
-	var ajax = {};
-
 	function init() {
 		$(document).on(
 			"click",
 			".udb-review-notice.is-permanent-dismissible .notice-dismiss",
-			ajax.saveDismissal
+			saveDismissal
 		);
 		$(document).on(
 			"click",
 			".udb-bfcm-notice.is-permanent-dismissible .notice-dismiss",
-			ajax.saveDismissal
+			saveDismissal
 		);
 	}
 
-	ajax.saveDismissal = function (e) {
+	/**
+	 * Save dismissal.
+	 *
+	 * @param {JQuery.ClickEvent} e
+	 * @this {HTMLElement}
+	 */
+	function saveDismissal(e) {
 		$.ajax({
-			url: ajaxurl,
+			url: window.ajaxurl,
 			type: "post",
 			data: {
-				action: this.parentNode.dataset.ajaxAction,
+				action: this.parentElement?.dataset.ajaxAction,
 				dismiss: 1,
 			},
 		}).always(function (r) {
 			if (r.success) console.log(r.data);
 		});
-	};
+	}
 
 	init();
 })(jQuery);
