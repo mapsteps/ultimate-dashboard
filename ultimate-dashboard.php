3d30bc42dac6c33caecc845f06931fc9d9783b0a
mapsteps <connect@mapsteps.com>
2026-02-25 11:28:44 +0100

    6 years of support for this is enough

--- ultimate-dashboard.php
+++ ultimate-dashboard.php
@@ -21,45 +21,6 @@ define( 'ULTIMATE_DASHBOARD_PLUGIN_URL', rtrim( plugin_dir_url( __FILE__ ), '/' 
 define( 'ULTIMATE_DASHBOARD_PLUGIN_VERSION', '3.8.13' );
 define( 'ULTIMATE_DASHBOARD_PLUGIN_FILE', plugin_basename( __FILE__ ) );
 
-/**
- * Hack to fix broken plugin updater in Ultimate Dashboard PRO 3.0.
- * This will be removed with a future update.
- */
-if ( defined( 'ULTIMATE_DASHBOARD_PRO_PLUGIN_VERSION' ) && version_compare( ULTIMATE_DASHBOARD_PRO_PLUGIN_VERSION, '3.0', '==' ) ) {
-
-	/**
-	 * Plugin updater function.
-	 */
-	function udb_pro_plugin_updater_helper() {
-
-		// To support auto-updates, this needs to run during the wp_version_check cron job for privileged users.
-		$doing_cron = defined( 'DOING_CRON' ) && DOING_CRON;
-		if ( ! current_user_can( 'manage_options' ) && ! $doing_cron ) {
-			return;
-		}
-
-		// Retrieve our license key from the DB.
-		$license_key = trim( get_option( 'ultimate_dashboard_license_key' ) );
-
-		// Setup the updater.
-		$edd_updater = new EDD_SL_Plugin_Updater(
-			ULTIMATE_DASHBOARD_PRO_STORE_URL,
-			ULTIMATE_DASHBOARD_PRO_PLUGIN_DIR . '\ultimate-dashboard-pro.php',
-			array(
-				'version' => ULTIMATE_DASHBOARD_PRO_PLUGIN_VERSION,
-				'license' => $license_key,
-				'item_id' => ULTIMATE_DASHBOARD_PRO_ITEM_ID,
-				'author'  => 'David Vongries',
-				'beta'    => false,
-			)
-		);
-
-	}
-
-	add_action( 'init', 'udb_pro_plugin_updater_helper' );
-
-}
-
 // Admin menu specific support.
 require_once __DIR__ . '/modules/admin-menu/inc/not-doing-ajax.php';
 udb_admin_menu_not_doing_ajax();
